7956376184723359b3073e22ddf86638c273235d
March0o <martynascep9@gmail.com>
2024-05-14 16:34:22 +0100

    Added some commenting and re-read over code

--- server/routes/posts.mjs
+++ server/routes/posts.mjs
@@ -5,11 +5,12 @@ const router = express.Router();
 
 router.get("/users/login", async (req, res) => {
   try {
+    //  Declare Variables
     const { name, password } = req.query;
-    const collection = await db.collection("users");
+    const collection = await db.collection("users"); // Initialize MongoDB db
     const query = { _name: name, _password: password };
 
-    const result = await collection.findOne(query);
+    const result = await collection.findOne(query); // Check if user exists
 
     if (!result) {
       res.status(401).send("No Users Found With Matching Credentials");
@@ -24,16 +25,15 @@ router.get("/users/login", async (req, res) => {
 
 router.post("/users/signup", async (req, res) => {
   try {
-    const newUser = req.body; // Assuming the request body contains user data
-    const collection = await db.collection("users");
+    // Declare Variables
+    const newUser = req.body;
+    const collection = await db.collection("users"); // Initialize MongoDB db
     
-    // Check if user already exists
     const existingUser = await collection.findOne({ _name: newUser._name });
     if (existingUser) {
       return res.status(409).send("User already exists");
     }
 
-    // Insert the new user
     const result = await collection.insertOne(newUser);
     res.status(201).send("User created successfully");
   } catch (error) {
@@ -45,12 +45,11 @@ router.post("/users/signup", async (req, res) => {
 router.post("/savedRecipes/add", async (req, res) => {
   try {
     const { _user_id, _recipe_id, _recipe_name, _recipe_thumbnail } = req.body;
-
     if (!_user_id || !_recipe_id || !_recipe_name || !_recipe_thumbnail) {
       return res.status(400).send("All fields are required");
     }
 
-    const existingRecipe = await db.collection("savedRecipes").findOne({ _user_id, _recipe_id });
+    const existingRecipe = await db.collection("savedRecipes").findOne({ _user_id, _recipe_id }); // Check if exitsts
 
     if (existingRecipe) {
       return res.status(400).send("This recipe is already saved");
@@ -58,7 +57,7 @@ router.post("/savedRecipes/add", async (req, res) => {
 
     const newSavedRecipe = { _user_id, _recipe_id, _recipe_name, _recipe_thumbnail };
 
-    const collection = await db.collection("savedRecipes");
+    const collection = await db.collection("savedRecipes"); // Initialize MongoDB db
     const result = await collection.insertOne(newSavedRecipe);
 
     if (result.acknowledged && result.insertedId) {
@@ -75,19 +74,15 @@ router.post("/savedRecipes/add", async (req, res) => {
 // Delete liked recipe
 router.delete("/savedRecipes/delete", async (req, res) => {
   try {
-    // Extract the recipe ID and user ID from the request body
     const { user_id, recipe_id } = req.body;
 
-    // Check if both user ID and recipe ID are provided
     if (!user_id || !recipe_id) {
       return res.status(400).send("Both user ID and recipe ID are required");
     }
 
-    // Find and delete the liked recipe
-    const collection = await db.collection("savedRecipes");
+    const collection = await db.collection("savedRecipes"); // Initialize MongoDB db
     const result = await collection.deleteOne({ _user_id: user_id, _recipe_id: recipe_id });
 
-    // Check if the deletion was successful
     if (result.deletedCount === 1) {
       return res.status(200).send("Liked recipe deleted successfully");
     } else {
@@ -103,16 +98,14 @@ router.get("/savedRecipes/check", async (req, res) => {
   try {
     const { userId, recipeId } = req.query;
 
-    // Basic validation to ensure required parameters are provided
     if (!userId || !recipeId) {
       return res.status(400).send("User ID and recipe ID are required");
     }
 
-    // Check if the user has already liked the recipe
     const existingLike = await db.collection("savedRecipes").findOne({
       _user_id: userId,
       _recipe_id:  Number.parseInt(recipeId)
-    });
+    }); // Check if exists
 
     if (existingLike) {
       return res.send(true); // User has already liked the recipe
@@ -132,7 +125,7 @@ router.post("/savedRecipes", async (req, res) => {
       return res.status(400).send("User ID is required");
     }
 
-    const collection = await db.collection("savedRecipes");
+    const collection = await db.collection("savedRecipes"); // Initialize MongoDB db
     const query = { _user_id };
     const results = await collection.find(query).toArray();
     
